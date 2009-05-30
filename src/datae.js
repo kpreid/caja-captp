ae10a430a2da373ef7170adb45c195f5f985b9c1
kpreid <kpreid@mac.com>
2009-05-30 00:47:14 +0000

    Minimal implementations of all builder methods for deJSONTreeKit.

--- src/datae.js
+++ src/datae.js
@@ -1,22 +1,59 @@
+
+// This performs the same algorithm as deASTKit from E-on-Java, with only difference in the output.
 var deJSONTreeKit = (function () {
   return {
     toString: function () { return "deJSONTreeKit"; },
-    makeBuilder: function () { 
+    makeBuilder: function () {
+      var nextTemp = 0;
       return {
         toString: function () { return "<deJSONTreeKit builder>"; },
         
         buildRoot: function (node) {
+          nextTemp = null; // help discourage accidental reuse
           return node;
         },
         
         buildLiteral: function (literal) {
           var t = typeof(literal);
           if (t === "string") {
-            return cajita.freeze(["string", literal]);
+            return literal;
           } else if (t === "number") {
             // XXX review whether we would like to support explicit ints
             return cajita.freeze(["float64", literal]);
           }
+        },
+
+        buildImport: function (varName) {
+          return cajita.freeze(["import", varName]);
+        },
+        
+        buildIbid: function (index) {
+          // XXX range check
+          return cajita.freeze(["ibid", index]);
+        },
+        
+        buildCall: function (recipient, verb, args) {
+          return cajita.freeze(["call", recipient, verb, args]);
+        },
+        
+        buildPromise: function () {
+          var promiseIndex = nextTemp;
+          nextTemp += 2;
+          //varReused
+          return promiseIndex;
+        },
+        
+        buildDefine: function (rValue) {
+          var tempIndex = nextTemp++;
+          //varReused[tempIndex] = false; // XXX implement the deASTKit functionality this corresponds to
+          var defExpr = cajita.freeze(["define", tempIndex, rValue]);
+          return cajita.freeze([defExpr, tempIndex]);
+        },
+        
+        buildDefrec: function (resolverIndex, rValue) {
+          var promiseIndex = resolverIndex - 1;
+          var defExpr = cajita.freeze(["defrec", promiseIndex, rValue]);
+          return defExpr;
         },
         
       }; // end builder
