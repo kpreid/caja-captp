d9a6b20b7dfa53a167ce77b8ce2c3d70f0ba7eea
kpreid@mac.com <kpreid@mac.com@8e81bbee-2eb9-11de-874f-bb9810f3bc79>
2009-08-06 03:59:21 +0000

    Stub implementations of sameYet and sameEver for later use. Moved identical() to util.js.

--- trunk/src/util.js
+++ trunk/src/util.js
@@ -2,6 +2,23 @@
 // Copyright 2009 Kevin Reid under the terms of the MIT X license
 // found at http://www.opensource.org/licenses/mit-license.html ...............
 
+/**
+ * Are x and y not observably distinguishable?
+ * 
+ * -- Copied from cajita.js until such time as it exposes this function itself.
+ */
+function identical(x, y) {
+  if (x === y) {
+    // 0 === -0, but they are not identical
+    return x !== 0 || 1/x === 1/y;
+  } else {
+    // NaN !== NaN, but they are identical.
+    // NaNs are the only non-reflexive value, i.e., if x !== x,
+    // then x is a NaN.
+    return x !== x && y !== y;
+  }
+}
+
 function once(func) {
   var live = true;
   function onceFunc() {
@@ -17,5 +34,6 @@ function once(func) {
 
 // exports
 cajita.freeze({
+  "identical": identical,
   "once": once
 });
