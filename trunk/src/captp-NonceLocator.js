e32a2dd01ded436f59f97283e661cffd9d04a244
kpreid@switchb.org <kpreid@switchb.org@8e81bbee-2eb9-11de-874f-bb9810f3bc79>
2010-09-01 18:03:39 +0000

    Update for Caja changes: replace "use strict,cajita"; with "use cajita";

--- trunk/src/captp-NonceLocator.js
+++ trunk/src/captp-NonceLocator.js
@@ -1,4 +1,4 @@
-"use strict,cajita";
+"use cajita";
 // Copyright 2002 Combex, Inc. under the terms of the MIT X license
 // found at http://www.opensource.org/licenses/mit-license.html ...............
 
